c46703029f4359a9a9038b417bbec3db78c5bef7
Erk Ekin <erk.ekin@aexp.com>
2022-12-28 11:42:18 +0000

    adds python code and executes

---
+++ train.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from os import EX_OK, EX_USAGE
+import subprocess
+import sys
+from pathlib import Path
+from python_utilities import lipo
+
+
+print("hello")
