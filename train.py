a481bd81fcec2f35949df9cfa26279b97b9a57bb
Erk Ekin <erk.ekin@aexp.com>
2022-12-28 13:12:59 +0000

    get_current_submodule_hash

--- train.py
+++ train.py
@@ -5,11 +5,10 @@ from os import EX_OK, EX_USAGE
 import subprocess
 
 def get_current_submodule_hash():
-    result = subprocess.run(['gh', 'api', '-H', 'Accept: application/vnd.github+json', "/repos/erkekin/submodule-project-parent/contents/submodule-project-child", '-q', '.sha'], capture_output=True, check=False)
+    result = subprocess.run(['gh', 'api', '-H', 'Accept: application/vnd.github+json', "/repos/erkekin/submodule-project-parent/contents/submodule-project-child", '-q', '.sha'], capture_output=True, check=True)
     if result.returncode == EX_OK:
         return result.stdout.decode('utf-8').strip()
     else:
         print(result.stderr)
 
-print("hello")
-get_current_submodule_hash()
+print(get_current_submodule_hash())
